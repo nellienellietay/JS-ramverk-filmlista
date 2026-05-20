4235c439df4f81438e4cc994454d76a39b445243
nellienellietay <nellie.elisabeth@gmail.com>
2026-05-20 21:06:45 +0200

    Added a requirement
    Rensar formuläret efter att film laddas upp. Även lagt in placeholder på titelrutan.

--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -22,6 +22,9 @@ function AddMovieForm({ addMovie }) {
         };
 
         addMovie(movie);
+
+        setTitle("");
+        setRating("0");
     }
     
     return (
@@ -30,12 +33,12 @@ function AddMovieForm({ addMovie }) {
 
                 <legend>Läg till en film</legend>
                 <label htmlFor="title-field">Titel:</label>
-                <input type= "text" id="title-field" className="form-control" value={title} onChange={(event) => setTitle(event.target.value)}/>
+                <input type= "text" id="title-field" className="form-control" placeholder="Skriv din filmtitel här..."value={title} onChange={(event) => setTitle(event.target.value)}/>
 
                 <label htmlFor="rating-field">Betyg:</label>
                 <select id="rating-field" className="form-control" value={rating} onChange={(event) => setRating(event.target.value)} >
 
-                    <option value="0">Välj betyg här</option>
+                    <option value="0">Välj betyg här...</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
